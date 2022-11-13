f6e6991ea38debb360eb65f00d438feeabb91448
Shuxin-Lai <shuxinlai001@gmail.com>
2022-11-13 14:07:02 +0800

    chore: add swagger

--- src/main/java/com/imooc/mall/controller/CategoryController.java
+++ src/main/java/com/imooc/mall/controller/CategoryController.java
@@ -7,6 +7,7 @@ import com.imooc.mall.model.pojo.User;
 import com.imooc.mall.model.request.category.AddCategoryReq;
 import com.imooc.mall.service.CategoryService;
 import com.imooc.mall.service.UserService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,6 +27,7 @@ public class CategoryController {
   @Autowired
   CategoryService categoryService;
 
+  @ApiOperation("后台添加商品目录")
   @PostMapping("/admin/add")
   @ResponseBody
   public ApiRestResponse addCategory(HttpSession session, @Valid @RequestBody AddCategoryReq addCategoryReq) {
